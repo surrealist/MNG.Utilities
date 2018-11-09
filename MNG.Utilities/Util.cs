3d0cfe81ddea0431b656d2992f7dfa4cc1b0d817
Suthep Sangvirotjanaphat <suthep@gfbiz.net>
2018-11-09 10:41:47 +0700

    Initial
    First commit.

---
+++ MNG.Utilities/Util.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace MNG.Utilities
+{
+  /// <summary>
+  /// 1,a,2,3,5
+  /// Util.WriteFile("product.csv", 1, "a", 2, 3, 5);
+  /// </summary>
+  public static class Util
+  {
+
+    public static void AppendFile(string fileName, params object[] data)
+    {
+      if (data == null) return;
+      if (data.Length == 0) return;
+
+      var text = string.Join(",", data) + Environment.NewLine;
+
+      File.AppendAllText(fileName, text);
+    }
+  }
+}
